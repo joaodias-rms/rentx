8ebcd52650b590ef0af265b2324f21524d1f9932
joao <joao.victor@topsapp.com.br>
2021-11-17 17:23:02 -0400

    criado arquivo de tema global

---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    header: "#1B1B1F",
+
+    background_primary: "#F4F5F6",
+    background_secondary: "#FFF",
+
+    text: "#7A7A80",
+    text_detail: "#AEAEB3",
+    title: "#47474D",
+
+    line: "#EBEBF0",
+
+    main: "#CD1637",
+    main_light: "#FDEEDF",
+    success: "#03B252",
+
+    shape: "#E1E1E8",
+    shape_dark: "#29292E",
+  },
+  fonts: {
+    primary400: "Inter_400Regular",
+    primary500: "Inter_500Medium",
+    secondary400: "Archivo_400Regular",
+    secondary500: "Archivo_500Medium",
+    secondary600: "Archivo_600SemiBold",
+  },
+};
