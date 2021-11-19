0f80adcdbf83187ad9e7b4e8f3fa0aabe0c376a6
joao <jvd.ramos@hotmail.com>
2021-11-19 11:24:41 -0400

    criada tela de detalhes do agendamento

---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+
+import { ImageSlider } from "../../components/ImageSlider";
+import { BackButton } from "../../components/BackButton";
+import { Acessory } from "../../components/Acessory";
+import { Button } from "../../components/Button";
+
+import SpeedSVG from "../../assets/speed.svg";
+import ForceSVG from "../../assets/force.svg";
+import PeopleSVG from "../../assets/people.svg";
+import GasolineSVG from "../../assets/gasoline.svg";
+import ExchangeSVG from "../../assets/exchange.svg";
+import AccelerationSVG from "../../assets/acceleration.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Acessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetail,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from "./styles";
+import theme from "../../styles/theme";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export function SchedulingDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://static.wikia.nocookie.net/forzamotorsport/images/4/45/HOR_XB1_Audi_RS_5.png/revision/latest?cb=20191201154056",
+          ]}
+        />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>BMW</Brand>
+            <Name>M3</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$580</Price>
+          </Rent>
+        </Details>
+        <Acessories>
+          <Acessory name="380 Km/h" icon={SpeedSVG} />
+          <Acessory name="3.2s" icon={AccelerationSVG} />
+          <Acessory name="800hp" icon={ForceSVG} />
+          <Acessory name="Gasolina" icon={GasolineSVG} />
+          <Acessory name="Auto" icon={ExchangeSVG} />
+          <Acessory name="2" icon={PeopleSVG} />
+        </Acessories>
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+          <DateInfo>
+            <DateTitle>De</DateTitle>
+            <DateValue>18/11/2021</DateValue>
+          </DateInfo>
+
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+
+          <DateInfo>
+            <DateTitle>Até</DateTitle>
+            <DateValue>18/11/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+        <RentalPrice>
+          <RentalPriceLabel>Total</RentalPriceLabel>
+          <RentalPriceDetail>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$2.900</RentalPriceTotal>
+          </RentalPriceDetail>
+        </RentalPrice>
+      </Content>
+      <Footer>
+        <Button title="Alugar agora" color={theme.colors.success} />
+      </Footer>
+    </Container>
+  );
+}
