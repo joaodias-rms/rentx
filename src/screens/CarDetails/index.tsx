ba9813731d1e8bfb85a4cbafb8f53a8fac740eef
joao <jvd.ramos@hotmail.com>
2021-11-24 15:36:11 -0400

    transferido parametro car pela rota

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -38,7 +38,7 @@ export function CarDetails() {
   const { car } = route.params as Params;
 
   function handleChooseRentalPeriod() {
-    navigation.navigate("Scheduling");
+    navigation.navigate("Scheduling", car);
   }
 
   function handleGoBack() {
