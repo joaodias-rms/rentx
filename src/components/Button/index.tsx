85b96f9ff2655e5874158d85cb3be0d086e786df
joao <jvd.ramos@hotmail.com>
2021-11-25 16:58:50 -0400

    refatorado componente

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { RectButtonProps } from "react-native-gesture-handler";
-
+import { ActivityIndicator } from "react-native";
 import { Container, Title } from "./styles";
+import theme from "../../styles/theme";
 
 interface Props extends RectButtonProps {
   title: string;
   color?: string;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ title, color, ...rest }: Props) {
+export function Button({
+  loading = false,
+  enabled = true,
+  title,
+  color,
+  ...rest
+}: Props) {
   return (
-    <Container color={color} {...rest}>
-      <Title>{title}</Title>
+    <Container
+      style={{ opacity: enabled == false || loading === true ? 0.5 : 1 }}
+      enabled={enabled}
+      color={color}
+      {...rest}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
