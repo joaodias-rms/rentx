c02bc6cbbcf6810d0fd76d591f02b1a88a6c1d46
joao <jvd.ramos@hotmail.com>
2021-11-25 16:59:24 -0400

    criado botão de listar agendamentos

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,6 +2,7 @@ import { carDTO } from "../../dtos/carDTO";
 import { FlatList, FlatListProps } from "react-native";
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { RectButton } from "react-native-gesture-handler";
 
 export const Container = styled.View`
   flex: 1;
@@ -36,3 +37,17 @@ export const CarList = styled(
   },
   showsVerticalScrollIndicator: false,
 })``;
+
+export const MyCarsButton = styled(RectButton)`
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 13px;
+  right: 22px;
+
+  border-radius: 30px;
+
+  background-color: ${({ theme }) => theme.colors.main};
+`;
