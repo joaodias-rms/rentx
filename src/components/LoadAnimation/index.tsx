b49154815f4864ed32eac83ce7a1ce26acac0053
joao <jvd.ramos@hotmail.com>
2021-12-01 10:46:07 -0400

    criado componente de load animado

---
+++ src/components/LoadAnimation/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import LottieView from "lottie-react-native";
+
+import load_animated from "../../assets/load_animated.json";
+
+import { Container } from "./styles";
+
+export function LoadAnimation() {
+  return (
+    <Container>
+      <LottieView
+        resizeMode="contain"
+        autoPlay
+        source={load_animated}
+        style={{ height: 200 }}
+        loop
+      />
+    </Container>
+  );
+}
