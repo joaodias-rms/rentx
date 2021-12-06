61690839e660d0f718e6468aaa8858ea7500688d
joao <jvd.ramos@hotmail.com>
2021-12-06 10:17:31 -0400

    refatorado estilização

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,25 +1,42 @@
+import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
+
+interface Props {
+  isFocused: boolean;
+}
 
 export const Container = styled.View`
   flex-direction: row;
   margin-bottom: 8px;
 `;
 
-export const IconContainer = styled.View`
+export const IconContainer = styled.View<Props>`
   background-color: ${({ theme }) => theme.colors.background_secondary};
   width: 55px;
   height: 55px;
   justify-content: center;
   align-items: center;
   margin-right: 2px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
 
-export const InputText = styled.TextInput`
+export const InputText = styled.TextInput<Props>`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background_secondary};
   padding: 0 23px;
   color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.primary400};
   font-size: ${RFValue(15)}px;
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
