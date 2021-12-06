3bccaf2dd701fd609ed3a4cb41cebbef07e0d83b
joao <jvd.ramos@hotmail.com>
2021-12-06 10:18:09 -0400

    adicionado verificação de foco do item

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInputProps } from "react-native";
 
 import { Feather } from "@expo/vector-icons";
@@ -8,15 +8,37 @@ import theme from "../../styles/theme";
 
 interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function Input({ iconName, ...rest }: InputProps) {
+export function Input({ iconName, value, ...rest }: InputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  function handleInputFocused() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value)
+  }
+
   return (
     <Container >
-      <IconContainer>
-        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={24}
+          color={isFocused || isFilled ? theme.colors.main : theme.colors.text_detail}
+        />
       </IconContainer>
-      <InputText {...rest}/>
+      <InputText
+        {...rest}
+        onFocus={handleInputFocused}
+        onBlur={handleInputBlur}
+        isFocused={isFocused}
+      />
     </Container>
   );
 }
