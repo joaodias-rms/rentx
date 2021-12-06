3aba18d4599c37d046b8ca98f3826af81cdf638d
joao <jvd.ramos@hotmail.com>
2021-12-06 10:17:03 -0400

    adicionado validação de form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { StatusBar } from "react-native";
+import React, { useState } from "react";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import * as Yup from "yup";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -10,51 +17,78 @@ import theme from "../../styles/theme";
 import { Container, Header, Title, Subtitle, Footer, Form } from "./styles";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("Email Obrigatório")
+          .email("Digite um Email válido"),
+        password: Yup.string().required("Senha obrigatória"),
+      });
+  
+      await schema.validate({email, password})  
+    } catch(error){
+      
+    }
+    
+  }
+
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Header>
-        <Title>Estamos {"\n"}quase lá.</Title>
-        <Subtitle>
-          Faça seu login para começar{"\n"}uma experiência incrível.
-        </Subtitle>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Header>
+            <Title>Estamos {"\n"}quase lá.</Title>
+            <Subtitle>
+              Faça seu login para começar{"\n"}uma experiência incrível.
+            </Subtitle>
+          </Header>
 
-      <Form>
-        <Input
-          iconName="mail"
-          autoCorrect={false}
-          autoCapitalize="none"
-          keyboardType="email-address"
-          placeholder="Email"
-        />
-        <PasswordInput
-          iconName="lock"
-          placeholder="Senha"
-          autoCapitalize="none"
-        />
-      </Form>
+          <Form>
+            <Input
+              iconName="mail"
+              autoCorrect={false}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              placeholder="Email"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              autoCapitalize="none"
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
 
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          enabled
-          loading={false}
-          color={theme.colors.background_secondary}
-          text_color={theme.colors.header}
-        />
-      </Footer>
-    </Container>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={handleSignIn}
+              enabled={true}
+              loading={false}
+            />
+            <Button
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              enabled
+              loading={false}
+              color={theme.colors.background_secondary}
+              text_color={theme.colors.header}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
