e10875dcfafa20beced2c48f161ccf0bed41aa33
joao <jvd.ramos@hotmail.com>
2021-12-06 10:18:19 -0400

    adicionado foco no componente

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -13,10 +13,23 @@ import theme from "../../styles/theme";
 
 interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function PasswordInput({ iconName, ...rest }: InputProps) {
+export function PasswordInput({ iconName, value, ...rest }: InputProps) {
   const [passwordVisible, setPasswordVisible] = useState(true);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  function handleInputFocused() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }
 
   function handlePasswordVisibilityChange() {
     setPasswordVisible((prevState) => !prevState);
@@ -24,12 +37,27 @@ export function PasswordInput({ iconName, ...rest }: InputProps) {
 
   return (
     <Container>
-      <IconContainer>
-        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={24}
+          color={
+            isFocused || isFilled ? theme.colors.main : theme.colors.text_detail
+          }
+        />
       </IconContainer>
-      <InputText {...rest} secureTextEntry={passwordVisible}/>
+      <InputText
+        {...rest}
+        secureTextEntry={passwordVisible}
+        onFocus={handleInputFocused}
+        onBlur={handleInputBlur}
+        isFocused={isFocused}
+      />
 
-      <ChangePasswordVisible onPress={handlePasswordVisibilityChange}>
+      <ChangePasswordVisible
+        isFocused={isFocused}
+        onPress={handlePasswordVisibilityChange}
+      >
         <Feather
           size={24}
           name={passwordVisible ? "eye" : "eye-off"}
