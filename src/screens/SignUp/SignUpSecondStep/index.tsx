05aea704faf9761b2e500e5f5d94d9015fd6ad01
joao <jvd.ramos@hotmail.com>
2021-12-07 17:35:01 -0400

    criada tela pra cadastro de senha

---
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import { BackButton } from "../../../components/BackButton";
+import { Bullet } from "../../../components/Bullet";
+import { PasswordInput } from "../../../components/PasswordInput";
+import { Button } from "../../../components/Button";
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle,
+} from "./styles";
+import theme from "../../../styles/theme";
+import { useState } from "react";
+import api from "../../../services/api";
+
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driversLicense: string;
+  };
+}
+
+export function SignUpSecondStep() {
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const navigation = useNavigation();
+
+  const route = useRoute();
+
+  const { user } = route.params as Params;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  async function handleRegister() {
+    if (!password || !passwordConfirm) {
+      return Alert.alert(
+        "Senha não informada",
+        "Por favor preencha a senha e confirme"
+      );
+    } else if (password !== passwordConfirm) {
+      return Alert.alert(
+        "Senhas diferem",
+        "Favor verifique a senha informada e tente novamente"
+      );
+    }
+
+    await api
+      .post("/users", {
+        name: user.name,
+        email: user.email,
+        driver_license: user.driversLicense,
+        password,
+      })
+      .then(() => {
+        navigation.navigate("Confirmation", {
+          nextScreenRoute: "SignIn",
+          title: "Conta Criada",
+          message: `Agora é só fazer login ${"\n"} e aproveitar`,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        
+        Alert.alert("Opa", "Não foi possível cadastrar");
+      });
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleGoBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+          <Title>Crie sua {"\n"}conta</Title>
+          <SubTitle>Faça seu cadastro de {"\n"}forma rápida e fácil</SubTitle>
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Confirme sua senha"
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
+            />
+          </Form>
+          <Button
+            title="Cadastrar"
+            color={theme.colors.success}
+            onPress={handleRegister}
+          />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
