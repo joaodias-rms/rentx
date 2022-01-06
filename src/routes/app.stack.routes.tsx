21ef6ec92c713f17a4dc9548b28e831a987c8a49
joao <joao.victor@topsapp.com.br>
2022-01-06 08:50:40 -0400

    refatorado rota por pilha

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -9,29 +9,19 @@ import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { Confirmation } from "../screens/Confirmation";
-import { SignIn } from "../screens/SignIn";
-import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
-import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function StackRoutes() {
+export function AppStackRoutes() {
   return (
-    <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
-      <Screen name="Splash" component={Splash} />
-      <Screen
-        name="Home"
-        component={Home}
-        options={{ gestureEnabled: false }}
-      />
+    <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      {/* <Screen name="Splash" component={Splash} /> */}
+      <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
       <Screen name="Confirmation" component={Confirmation} />
       <Screen name="MyCars" component={MyCars} />
-      <Screen name="SignIn" component={SignIn} />
-      <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
-      <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
     </Navigator>
   );
 }
