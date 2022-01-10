f6bd350707eda0d9db26ab88c0d9f76af5ee29ad
joao <jvd.ramos@hotmail.com>
2022-01-10 17:31:01 -0400

    refatorado hook de autenticação

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,17 +1,17 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
+import { database } from "../database";
 import api from "../services/api";
+
+import { User as ModelUser } from "../database/models/User";
 
 interface User {
   id: string;
+  user_id: string;
   eamil: string;
   name: string;
   driver_license: string;
   avatar: string;
-}
-
-interface AuthState {
   token: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -31,21 +31,37 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<User>({} as User);
 
   async function signIn({ email, password }: SignInCredentials) {
-    const response = await api.post("/sessions", {
-      email,
-      password,
-    });
+    try {
+      const response = await api.post("/sessions", {
+        email,
+        password,
+      });
 
-    const { token, user } = response.data;
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      const { token, user } = response.data;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-    setData({ token, user });
+      const userCollection = database.get<ModelUser>("users");
+      await database.action(async () => {
+        await userCollection.create((newUser) => {
+          (newUser.user_id = user.id),
+            (newUser.name = user.name),
+            (newUser.email = user.email),
+            (newUser.driver_license = user.driver_license),
+            (newUser.avatar = user.avatar),
+            (newUser.token = token);
+        });
+      });
+
+      setData({ token, ...user });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data, signIn }}>
       {children}
     </AuthContext.Provider>
   );
