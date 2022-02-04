c3c6f03d69bf9b256a024444d6b07f2e2af3461c
joao <jvd.ramos@hotmail.com>
2022-02-04 17:29:46 -0400

    adicionado função de signout e update

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,14 +3,19 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { useNavigation } from "@react-navigation/native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useAuth } from "../../hooks/auth";
 
 import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
+
+import * as Yup from "yup";
 
 import {
   Container,
@@ -32,9 +37,12 @@ import { Feather } from "@expo/vector-icons";
 import theme from "../../styles/theme";
 
 export function Profile() {
-  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const { user, signOut, updateUser } = useAuth();
 
-  const { user } = useAuth();
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
 
   const navigation = useNavigation();
 
@@ -42,10 +50,69 @@ export function Profile() {
     navigation.goBack();
   };
 
-  const handleSignOut = () => {};
-
   const handleOptionChange = (selectedOption: "dataEdit" | "passwordEdit") => {
     setOption(selectedOption);
+  };
+
+  const handleSelectAvatar = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+    if (result.cancelled) {
+      return;
+    }
+    if (result.uri) {
+      setAvatar(result.uri);
+    }
+  };
+
+  const handleProfileUpdate = async () => {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string().required("CNH é obrigatória"),
+        name: Yup.string().required("Nome é obrigatório"),
+      });
+      const data = { name, driverLicense };
+      await schema.validate(data);
+
+      await updateUser({
+        id: user.id,
+        user_id: user.id,
+        email: user.email,
+        name,
+        driver_license: driverLicense,
+        avatar,
+        token: user.token,
+      });
+
+      Alert.alert("Perfil Atualizado");
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Opa", error.message);
+      }
+      Alert.alert("Erro", "Não foi possível atualizar o perfil");
+    }
+  };
+
+  const handleSignOut = () => {
+    Alert.alert(
+      "Tem certeza?",
+      "Se você sair, irá precisar de internet para retornar",
+      [
+        {
+          text: "Cancelar",
+          onPress: () => {},
+          style: "cancel",
+        },
+        { 
+          text: "Sair",
+          onPress: () => signOut()
+        },
+      ]
+    );
   };
 
   return (
@@ -61,8 +128,8 @@ export function Profile() {
               </LogoutButton>
             </HeaderTop>
             <PhotoContainer>
-              <Photo source={{ uri: "https://github.com/joaodias-rms.png" }} />
-              <PhotoButton onPress={() => {}}>
+              {!!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleSelectAvatar}>
                 <Feather name="camera" size={24} color={theme.colors.shape} />
               </PhotoButton>
             </PhotoContainer>
@@ -93,6 +160,7 @@ export function Profile() {
                   autoCapitalize="sentences"
                   autoCorrect={false}
                   defaultValue={user.name}
+                  onChangeText={setName}
                 />
                 <Input
                   iconName="mail"
@@ -107,6 +175,7 @@ export function Profile() {
                   defaultValue={user.driver_license}
                   placeholderTextColor={theme.colors.text_detail}
                   keyboardType="numeric"
+                  onChangeText={setDriverLicense}
                 />
               </Section>
             ) : (
@@ -128,6 +197,7 @@ export function Profile() {
                 />
               </Section>
             )}
+            <Button title="salvar alterações" onPress={handleProfileUpdate} />
           </Content>
         </Container>
       </TouchableWithoutFeedback>
