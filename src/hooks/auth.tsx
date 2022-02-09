8e98d8b1f2c9514438afdfbe8c6f3ab588f61144
joao <jvd.ramos@hotmail.com>
2022-02-09 16:14:58 -0400

    adicionado validação de carregamento

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -25,6 +25,7 @@ interface AuthContextData {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
   updateUser: (user: User) => Promise<void>;
+  loading: boolean
 }
 
 interface AuthProviderProps {
@@ -35,6 +36,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [data, setData] = useState<User>({} as User);
+  const [loading, setLoading] = useState(true)
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -105,13 +107,14 @@ function AuthProvider({ children }: AuthProviderProps) {
           "Authorization"
         ] = `Bearer ${userData.token}`;
         setData(userData);
+        setLoading(false)
       }
     }
     loadUserData();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data, signIn, signOut, updateUser }}>
+    <AuthContext.Provider value={{ user: data, signIn, signOut, updateUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,11 +6,14 @@ import { useAuth } from "../hooks/auth";
 
 import { AppTabRoutes } from "./app.tabs.routes";
 import { AuthRoutes } from "./auth.routes";
+import { LoadAnimation } from "../components/LoadAnimation";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  return (
+  return loading ? (
+    <LoadAnimation />
+  ) : (
     <NavigationContainer>
       {user.id ? <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
